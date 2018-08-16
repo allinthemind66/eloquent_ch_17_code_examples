c6aac6d12361cf3409ff51394ac69feff6e6793c
allinthemind66 <r.nilon92@gmail.com>
2018-08-16 16:23:31 -0400

    worked on curves

--- index.js
+++ index.js
@@ -35,13 +35,45 @@
 // }
 // cx.stroke();
 
-let cx = document.querySelector("canvas").getContext("2d");
-cx.beginPath();
-cx.moveTo(50, 10);
-cx.lineTo(10, 70);
-cx.lineTo(90, 70);
-// cx.lineTo(50, 10);
-//closePath finishes the path for you
+// let cx = document.querySelector("canvas").getContext("2d");
+// cx.beginPath();
+// cx.moveTo(50, 10);
+// cx.lineTo(10, 70);
+// cx.lineTo(90, 70);
+// // cx.lineTo(50, 10);
+// //closePath finishes the path for you
+// // cx.closePath();
+// // cx.stroke();
+// cx.fill();
+
+//CURVES
+//QUADRATIC CURVE
+// let cx = document.querySelector("canvas").getContext("2d");
+// cx.beginPath();
+// cx.moveTo(10, 90);
+// //control=(60, 10) goal=(90, 90)
+// //control point is the middle of the curve
+// cx.quadraticCurveTo(60, 10, 90, 90);
+// cx.lineTo(60, 10);
 // cx.closePath();
 // cx.stroke();
-cx.fill();
+
+// let cx = document.querySelector("canvas").getContext("2d");
+// cx.beginPath();
+// cx.moveTo(10, 90);
+// //control=(10, 10) control2=(90, 10) goal=(50, 90)
+// cx.bezierCurveTo(10,10,90,10,50,90);
+// cx.lineTo(90, 10);
+// cx.lineTo(10, 10);
+// cx.closePath();
+// cx.stroke();
+
+// let cx = document.querySelector("canvas").getContext("2d");
+// cx.beginPath();
+// //pair of coordinates for arcs center, a radius and then a start and end angle
+// //center=(50, 50) radius=40 angle=0 to 7
+// cx.arc(50, 50, 40, 0, 7 );
+// // center=(150, 50) radius=40 angle=0 to ½π
+// //THERE IS A LINE CONNECTING THESE TWO ARCS BECAUSE THEY ARE STILL THE SAME PATH
+// cx.arc(150, 50, 40, 0, 0.5 * Math.PI);
+// cx.stroke();

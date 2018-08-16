fc7088505ba140749d61ad8bea5a8f28e299a825
allinthemind66 <r.nilon92@gmail.com>
2018-08-16 15:38:06 -0400

    lines and surfaces

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-let circle = document.querySelector("circle");
-circle.setAttribute('fill', 'cyan');
+// ############### SVG CODE ###################
+// let circle = document.querySelector("circle");
+// circle.setAttribute('fill', 'cyan');
+
+//context is an object whose methods provide the drawing interface
+//two widely supported drawing styles:
+//"2d" for two dimensional and "webgl" for 3d graphics
+
+
+//################# CANVAS CODE #######################
+// let canvas = document.querySelector('canvas');
+// let context = canvas.getContext("2d");
+// context.fillStyle = "red";
+// //top left coordinates(x,y) followed by 100 px wide and 50 px high
+// context.fillRect(10, 10, 100, 50);
+
+let cx = document.querySelector("canvas").getContext("2d");
+  cx.strokeStyle = "blue";
+  cx.strokeRect(5, 5, 50, 50);
+  cx.lineWidth = 5;
+  cx.strokeRect(135, 5, 50, 50);
